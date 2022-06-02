a310f1a636a3e8bded1fd171f00d3f1eba4f78ff
RedCrafter07 <github@redcrafter07.de>
2022-06-02 20:07:32 +0200

    Copy assets

--- webpack.plugins.js
+++ webpack.plugins.js
@@ -1,3 +1,17 @@
 const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const path = require('path');
 
-module.exports = [new ForkTsCheckerWebpackPlugin()];
+const assets = ['assets'];
+const copyPlugins = assets.map((asset) => {
+	return new CopyWebpackPlugin({
+		patterns: [
+			{
+				from: path.resolve(__dirname, 'src', asset),
+				to: asset,
+			},
+		],
+	});
+});
+
+module.exports = [new ForkTsCheckerWebpackPlugin(), ...copyPlugins];

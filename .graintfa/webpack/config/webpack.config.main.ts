6c06547f96042149941d3b6029535a97430eebe0
RedCrafter07 <gh@r07.dev>
2024-05-09 14:39:24 +0200

    Finish configs

---
+++ .graintfa/webpack/config/webpack.config.main.ts
@@ -0,0 +1,27 @@
+import { Configuration } from 'webpack';
+import { EsbuildPlugin } from 'esbuild-loader';
+import RestartPlugin from '../plugins/RestartPlugin';
+
+export default {
+	entry: {
+		index: './src/main/index.ts',
+		preload: './src/main/preload.ts',
+	},
+	target: 'electron-main',
+	output: {
+		path: __dirname + '/../../dist/main',
+		filename: '[name].js',
+	},
+	module: {
+		rules: [
+			{
+				test: /\.ts$/,
+				use: 'esbuild-loader',
+			},
+		],
+	},
+	plugins: [new EsbuildPlugin({ minify: true }), new RestartPlugin()],
+	resolve: {
+		extensions: ['.ts', '.js'],
+	},
+} as Configuration;
---
+++ .graintfa/webpack/config/webpack.config.renderer.ts
@@ -0,0 +1,35 @@
+import { Configuration } from 'webpack';
+import HTMLWebpackPlugin from 'html-webpack-plugin';
+
+export default {
+	entry: './src/renderer/index.tsx',
+	target: ['web', 'electron-renderer'],
+
+	output: {
+		path: __dirname + '/../../dist/renderer',
+		filename: 'index.js',
+	},
+
+	module: {
+		rules: [
+			{
+				test: /\.tsx?$/,
+				use: 'esbuild-loader',
+			},
+			{
+				test: /\.css$/,
+				use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
+			},
+		],
+	},
+
+	resolve: {
+		extensions: ['.tsx', '.ts', '.js', '.css'],
+	},
+
+	plugins: [
+		new HTMLWebpackPlugin({
+			template: './src/renderer/index.html',
+		}),
+	],
+} as Configuration;
